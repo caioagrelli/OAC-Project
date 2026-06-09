417f22cdba5e0e4411c6ca4082e4d0fb44fbb32d
Thales Afonso Dornelas Gomes <tadg@cin.ufpe.br>
2026-06-09 12:46:07 -0300

    Mapeia os campos Funct3 e Funct7 para deslocamentos e SLTU

--- project/src/pl_alu_ctrl.sv
+++ project/src/pl_alu_ctrl.sv
@@ -32,7 +32,15 @@ module pl_alu_ctrl (
             2'b10: begin                // R-type: decodificar Funct
                 case (Funct3)
                     3'h0: Operation = Funct7[5] ? 4'd02 : 4'd01; // SUB ou ADD
+
+                    3'h1: Operation = 4'd07; // SLL e SLLI
+
+                    3'h3: Operation = 4'd10; // SLTU
+
                     3'h4: Operation = 4'd03;  // XOR
+
+                    3'h5: Operation = Funct7[5] ? 4'd09 : 4'd08; // SRA/SRAI (4'd09) ou SRL/SRLI (4'd08)
+
                     3'h6: Operation = 4'd04;  // OR
                     3'h7: Operation = 4'd05;  // AND
                     3'h2: Operation = 4'd11;  // SLT
